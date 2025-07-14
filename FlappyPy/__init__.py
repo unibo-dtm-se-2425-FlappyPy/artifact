f8a1fe03d22009e768ba5b2be7f8b87f3d8fa65b
Nima Boubanian <nima.boubanian@gmail.com>
2025-07-14 22:34:29 +0200

    Modify the package config torun the game as a module correctly.

--- FlappyPy/__init__.py
+++ FlappyPy/__init__.py
@@ -1,12 +1,8 @@
 import logging
 
-
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('FlappyPy')
 
-# this is the initial module of the app
-# this is executed whenever some client-code is calling `import FlappyPy` or `from FlappyPy import ...`
-# put the main classes here:
 class MyClass:
     def my_method(self):
         return "Hello buddy! You will play FlappyPy soon!"
